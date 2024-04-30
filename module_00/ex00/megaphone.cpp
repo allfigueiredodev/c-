e075e5b2c3c1b3f7617ea40ba564a430f7de29be
Allesson Figueiredo <allfigueiredo@outlook.com>
2024-04-30 13:36:47 -0300

    (fix) module 00, ex00

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -1,4 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aperis-p <aperis-p@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 13:33:32 by aperis-p          #+#    #+#             */
+/*   Updated: 2024/04/30 13:33:35 by aperis-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
+#include <string>
+#include <cctype>
+
+std::string ft_upper(const std::string str)
+{
+	int i = 0;
+	std::string	up;
+
+	while(str[i])
+	{
+		up += std::toupper(str[i]);
+		i++;
+	}
+	return (up);
+}
 
 int main (int argc, char **argv)
 {
@@ -9,22 +36,24 @@ int main (int argc, char **argv)
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else if (argc == 2)
-		std::cout << argv[1] << std::endl;
+	{
+		concatenated += ft_upper(argv[1]);
+		std::cout << concatenated << std::endl;
+	}
 	else
 	{
 		while(argc != 1)
 		{
 			if (argc == 2)
-				concatenated.append(argv[i]);
+				concatenated += ft_upper(argv[i]);
 			else
 			{
-				concatenated.append(argv[i]);
-				concatenated.append(" ");
+				concatenated += ft_upper(argv[i]);
+				concatenated += " ";
 			}
 			i++;
 			argc--;
 		}
-		transform(concatenated.begin(), concatenated.end(), concatenated.begin(), ::toupper);
 		std::cout << concatenated << std::endl;
 	}
 }
