78e36f9b2bc37982f98291d091a1442ed019fb62
allfigueiredodev <allfigueiredo@outlook.com>
2024-06-11 08:43:07 -0300

    (wip) module 06, ex00

--- module_06/main.cpp
+++ module_06/main.cpp
@@ -1,5 +1,11 @@
 #include "scalar.h"
 
 int main (int argc, char **argv){
+	if (argc != 2) {
+		errorln("Bad arguments");
+		println(CYAN << "Try this way: ./convert <data to be converted>");
+		return (1);
+	}
+
 	
 };
