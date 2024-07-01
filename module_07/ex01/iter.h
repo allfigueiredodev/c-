f67e85142ad5053a2ca7c26f93d614e18f7b0681
allfigueiredodev <allfigueiredo@outlook.com>
2024-06-30 21:57:47 -0300

    (feat) module 07, ex01 - done

---
+++ module_07/ex01/iter.h
@@ -0,0 +1,39 @@
+#ifndef ITER_H
+#define ITER_H
+
+#include <iostream>
+#include <string>
+
+#define println(x) std::cout << x << DFT << std::endl;
+#define errorln(x) std::cerr << RED << x << DFT << std::endl;
+
+#define BLUE_GREEN	"\033[32;44m"
+#define MAG_YE		"\033[33;45m"
+#define RED     	"\033[1;31m"
+#define GREEN   	"\033[1;32m"
+#define YELLOW 		"\033[1;33m"
+#define BLUE    	"\033[1;34m"
+#define MAGENTA    	"\033[1;35m"
+#define CYAN    	"\033[1;36m"
+#define WHITE    	"\033[1;37m"
+#define ORANGE		"\033[0;38;5;166m"
+#define GREY		"\033[38;5;244m"
+#define DFT   		"\033[0m"
+
+template<typename T>
+void squaring(T& a) {
+	println(GREEN << a << "^2 = " << a*a);
+}
+
+template<typename TARR>
+void iter(TARR* arr, int& size, void(*fptr)(TARR& data)) {
+	if (!arr || size <= 0){
+		errorln("Invalid parameters.");
+		return ;
+	}
+	for(int i = 0; i < size; i++){
+		fptr(arr[i]);
+	}
+}
+
+#endif
---
+++ module_07/ex01/main.cpp
@@ -0,0 +1,52 @@
+#include "iter.h"
+
+void InvalidArrayTest(void){
+	int size = 10;
+	int* arr = NULL;
+	iter<int>(arr, size, squaring<int>);
+}
+
+void ValidIntArrayTest(void){
+	int size = 10;
+	int* arr = new int[size];
+	for(int i = 0; i < size; i++) {
+		arr[i] = i;
+	}
+	iter<int>(arr, size, squaring<int>);
+}
+
+void ValidFloatArrayTest(void){
+	int size = 10;
+	float* arr = new float[size];
+	for(int i = 0; i < size; i++) {
+		arr[i] = i + 0.3f;
+	}
+	iter<float>(arr, size, squaring<float>);
+}
+
+void ValidDoubleArrayTest(void){
+	int size = 10;
+	double* arr = new double[size];
+	for(int i = 0; i < size; i++) {
+		arr[i] = i + 0.7;
+	}
+	iter<double>(arr, size, squaring<double>);
+}
+
+int main(void) {
+	println(CYAN << "***INVALID ARRAY TEST***")
+	std::cout << std::endl;
+	InvalidArrayTest();
+	std::cout << std::endl;
+	println(CYAN << "***VALID INT ARRAY TEST***")
+	std::cout << std::endl;
+ 	ValidIntArrayTest();
+	std::cout << std::endl;
+	println(CYAN << "***VALID FLOAT ARRAY TEST***")
+	std::cout << std::endl;
+ 	ValidFloatArrayTest();
+	std::cout << std::endl;
+	println(CYAN << "***VALID DOUBLE ARRAY TEST***")
+	std::cout << std::endl;
+ 	ValidDoubleArrayTest();
+}
